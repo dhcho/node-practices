a9f34f679460bf9c3a4bfedd0a6479f1722368e5
dhcho <oop6413@gmail.com>
2021-07-09 16:28:52 +0900

    2021.07.09 node module, server practice

---
+++ project-ex02/app-ex01.js
@@ -0,0 +1,9 @@
+/**
+ * douzone-dhcho-math.js 파일 모듈 테스트
+ */
+
+const dzMath = require('../douzone-dhcho-math/douzone-dhcho-math');
+
+console.log(dzMath.sum(1, 2, 3, 4));
+console.log(dzMath.max(-10, -20, -30, -40));
+console.log(dzMath.min(-1, -2, 0, 1, 2));
