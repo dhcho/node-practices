5f4938b94482d3f2274dfb3fb17063625afe5819
dhcho <oop6413@gmail.com>
2021-07-22 22:58:15 +0900

    2021.07.22 mysite node admin practice

--- mysite/routes/admin.js
+++ mysite/routes/admin.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const authorized = require('./authorized');
 const controller = require('../controllers/admin');
+const guestbookController = require('../controllers/admin/guestbook');
+const boardController = require('../controllers/admin/board');
+const userController = require('../controllers/admin/user');
 
 const router = express.Router();
 router.route('/').get(authorized('ADMIN'), controller.index);
 router.route('/update').post(authorized('ADMIN'), controller.update);
+
+router.route('/user').get(userController.index);
+router.route('/board').get(boardController.index);
+router.route('/guestbook').get(guestbookController.index);
 
 module.exports = router;
